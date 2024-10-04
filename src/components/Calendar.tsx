25aa227be279d82bdb8a7aa3854ccbdc4757bc72
marcospallone <marcospallone@outlook.it>
2024-10-04 23:11:38 +0200

    event in cell + fix date and mapping trips

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,11 +1,14 @@
 import FullCalendar from "@fullcalendar/react";
-import { Box } from "@mui/material";
+import { Box, List, ListItem, Typography, useMediaQuery } from "@mui/material";
 import * as React from "react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import { useEffect, useState } from "react";
 import itLocale from "@fullcalendar/core/locales/it";
 import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import theme from "@/theme/theme";
+import { formatDate } from "@fullcalendar/core";
 
 interface Trip {
   id: number;
@@ -16,6 +19,10 @@ interface Trip {
 
 const Calendar: React.FC = () => {
   const [trips, setTrips] = useState<any[]>([]);
+  const [dayTrips, setDayTrips] = useState<any[]>([]);
+  const [showTripList, setShowTripList] = useState(false);
+
+  const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
 
   useEffect(() => {
     fetchTrips();
@@ -28,45 +35,109 @@ const Calendar: React.FC = () => {
         throw new Error("Errore durante il recupero dei viaggi");
       }
       const data: Trip[] = await response.json();
-      const formattedEvents = data.map((trip) => ({
-        id: trip.id.toString(),
-        title: trip.tripTitle + " - " + trip.vehicleName,
-        start: trip.date,
-      }));
-
-      setTrips(formattedEvents);
+      console.log(data);
+      setTrips(data);
     } catch (error) {
       console.error(error);
     }
   };
 
-  const handleEventClick = (clickInfo: any) => {
-    alert(
-      `Evento: ${clickInfo.event.title}\nData inizio: ${clickInfo.event.start}`
-    );
+  const handleCellClick = (info: any) => {
+    const dateStr = info.date.toISOString().split("T")[0];
+    if (isMobile) {
+      const eventsOnDay = trips.filter((event) =>
+        event.date.startsWith(dateStr)
+      );
+      console.log(eventsOnDay);
+      setDayTrips(eventsOnDay);
+      setShowTripList(true);
+    }
   };
 
   const renderEventContent = (eventInfo: any) => {
-    console.log(eventInfo)
+    console.log(eventInfo);
     return (
       <div>
         <b>{eventInfo.timeText}</b>
         <br />
+        <b>OK</b>
         <span>{eventInfo.event.title}</span>
+      </div>
+    );
+  };
+
+  const handleCellContent = (info: any) => {
+    const dateStr = info.date.toISOString().split("T")[0];
+    const eventsOnDay = trips.filter((event) => event.date.startsWith(dateStr));
+    const eventCount =
+      eventsOnDay.length > 0 ? `${eventsOnDay.length} eventi` : "";
+
+    return (
+      <div>
+        <div>{info.dayNumberText}</div>
+        {eventCount && (
+          <div
+            className="event-count"
+            style={{ fontSize: "0.8em", color: "#ff5722" }}
+          >
+            {eventCount}
+          </div>
+        )}
       </div>
     );
   };
 
   return (
     <Box>
-      <FullCalendar
-        locale={itLocale}
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
-        events={trips}
-        eventClick={handleEventClick}
-        eventContent={renderEventContent}
-      />
+      <Box>
+        <FullCalendar
+          locale={itLocale}
+          plugins={[dayGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          events={trips}
+          dateClick={handleCellClick}
+          // eventClick={handleEventClick}
+          eventContent={renderEventContent}
+          dayCellContent={isMobile && handleCellContent}
+          eventDisplay={isMobile ? "none" : undefined}
+        />
+      </Box>
+      {showTripList && (
+        <List>
+          {dayTrips.map((trip, index) => {
+            const validDate = new Date(trip.date);
+            const formattedDay = new Intl.DateTimeFormat("it-IT", {
+              day: "2-digit",
+              month: "long",
+              year: "numeric",
+            }).format(validDate);
+
+            const formattedTime = new Intl.DateTimeFormat("it-IT", {
+              hour: "2-digit",
+              minute: "2-digit",
+              hour12: false,
+            }).format(validDate);
+            const date = `${formattedDay} - ${formattedTime}`;
+
+            return (
+              <ListItem>
+                <Box>
+                  <Box>
+                    <Typography>{trip.tripTitle}</Typography>
+                  </Box>
+                  <Box>
+                    <Typography>{date}</Typography>
+                  </Box>
+                  <Box>
+                    <Typography>{trip.driverName}</Typography>
+                  </Box>
+                </Box>
+              </ListItem>
+            );
+          })}
+        </List>
+      )}
+      <Box></Box>
     </Box>
   );
 };
