037f3bc46b1d0fc3d320483e62a1b5da6f16b135
marcospallone <marcospallone@outlook.it>
2024-11-12 14:52:43 +0100

    oneSignal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,28 +4,23 @@ import CssBaseline from "@mui/material/CssBaseline";
 import theme from "../theme/theme";
 import { AppProps } from "next/app";
 import Layout from "@/components/Layout";
-import '../styles/global.scss';
+import "../styles/global.scss";
 import { useEffect } from "react";
 import OneSignal from "react-onesignal";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     OneSignal.init({
-      appId: '52ba47df-9457-4f9d-b8cd-9c25fff35a96',
+      appId: "52ba47df-9457-4f9d-b8cd-9c25fff35a96",
       notifyButton: {
         enable: false,
       },
       autoresubscribe: true,
       allowLocalhostAsSecureOrigin: true,
-      
     });
-    OneSignal.Notifications.requestPermission().then((permission:any) => {
-      if (permission === 'granted') {
-      } else {
-      }
-    });
+    OneSignal.Notifications.requestPermission();
   }, []);
-  
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
