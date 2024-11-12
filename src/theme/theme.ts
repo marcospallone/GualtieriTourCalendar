3230d847f6bd364941bc53f7ae619da4422f5dec
marcospallone <marcospallone@outlook.it>
2024-11-12 18:00:35 +0100

    fix main color

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -4,7 +4,7 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#1976d2', 
+      main: '#fff', 
     },
     secondary: {
       main: '#dc004e', 
